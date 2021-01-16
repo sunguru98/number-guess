385cfc68bcc517bc5ab971471b3b47b6ea6a0f91
Sundeep Charan Ramkumar <72760920+sundeep-mavfarm@users.noreply.github.com>
2021-01-17 00:03:31 +0530

    Added bare bones layout using React Native components

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,21 +1,101 @@
 import React from "react";
-import { View, StyleSheet, Text, ViewStyle } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ViewStyle,
+  TextStyle,
+  BackHandler,
+  Button,
+  TextInput,
+} from "react-native";
 
 type HomePageProps = {};
 
-const { container } = StyleSheet.create<{ container: ViewStyle }>({
+const {
+  container,
+  title,
+  buttonsContainer,
+  button,
+  textInput,
+  textInputContainer,
+} = StyleSheet.create<{
+  container: ViewStyle;
+  title: TextStyle;
+  textInputContainer: ViewStyle;
+  textInput: TextStyle;
+  buttonsContainer: ViewStyle;
+  button: ViewStyle;
+}>({
   container: {
     flex: 1,
     padding: 20,
     justifyContent: "flex-start",
     alignItems: "center",
   },
+
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  textInputContainer: {
+    width: "100%",
+    justifyContent: "space-between",
+    padding: 15,
+    marginTop: 20,
+  },
+
+  textInput: {
+    marginVertical: 20,
+    padding: 15,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#353b48",
+    color: "#2f3640",
+    fontWeight: "500",
+    fontSize: 20,
+  },
+
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  button: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
 });
 
 const Home: React.FC<HomePageProps> = () => {
+  const handleChange = () => {};
+
   return (
     <View style={container}>
-      <Text>Enter a number to get started !!</Text>
+      <Text style={title}>Guess the right number !!</Text>
+      <View style={textInputContainer}>
+        <Text>Enter a number to get started</Text>
+        <TextInput
+          style={textInput}
+          onChange={handleChange}
+          textContentType="telephoneNumber"
+          keyboardType="numeric"
+        />
+        <View style={buttonsContainer}>
+          <View style={button}>
+            <Button
+              title="QUIT GAME"
+              onPress={BackHandler.exitApp}
+              color="red"
+            />
+          </View>
+          <View style={button}>
+            <Button title="START GAME" onPress={console.log} />
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
